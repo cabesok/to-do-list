0dbed36354871414036cc0cbfd723429a77b7908
Emiliano Costa <emiliano@MacBook-Air-de-Emiliano.local>
2022-09-13 17:57:22 -0300

    app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React from "react";
+import Form from './components/Form';
+import TaskList from './components/TaskList';
+import {Grid} from "@mui/material";
+import {useState} from 'react';
+import { ListProvider } from './context/ListContext';
 
 function App() {
+  
+  const [search, setSearch] = useState([])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <ListProvider>
+      <Grid 
+      container
+      direction= 'column'
+      alignItems= 'center'
+      maxWidth= 'sm' 
+      sx={{margin: 'auto'}}>
+
+        <h1>To Do List</h1>
+
+        <Form />
+        <TaskList />
+
+      </Grid>
+  </ListProvider>
+    
   );
 }
 
